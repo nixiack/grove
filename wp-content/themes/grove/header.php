dcee743e4ba2c0cc561d025aa9d66a8114aff29e
John Coy <johndavidcoy@gmail.com>
2013-07-18 15:41:12 -0500

    Added missing small-nav script to header
    Reads grove/js/small-menu.js for responsive nav.

--- wp-content/themes/grove/header.php
+++ wp-content/themes/grove/header.php
@@ -41,6 +41,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/js/small-menu.js" type="text/javascript"></script>
 </head>
 
 <body <?php body_class(); ?>>
