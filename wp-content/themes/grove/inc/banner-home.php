34ee634108c19a08abbfa28d31dbb1a9d960956d
John Coy <johndavidcoy@gmail.com>
2014-01-23 17:32:21 -0600

    Banner text change
    Set heading for better styling.

--- wp-content/themes/grove/inc/banner-home.php
+++ wp-content/themes/grove/inc/banner-home.php
@@ -1,7 +1,7 @@
 <?php if (get_option( 'grove_home_banner_text_setting' )) { ?>
 <div class="banner-outer">
         <div class="banner-inner">
-                <?php echo get_option( 'grove_home_banner_text_setting' ) ?>
+                <h1><?php echo get_option( 'grove_home_banner_text_setting' ) ?></h1>
         </div>
 </div>
 <?php } ?>
